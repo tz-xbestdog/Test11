5e0ed73c563e145f0bda0d928b2a840cb476889c
xbestdog <764579112@qq.com>
2019-12-07 14:51:28 +0800

    更新

--- app/src/main/java/com/example/test11/MainActivity.kt
+++ app/src/main/java/com/example/test11/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.example.test11
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.myku.interceptor.HttpKit
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        HttpKit.getInstance()
+
+    }
+
+    /**
+     * 快捷登陆
+     */
+    private fun login(phone: String, password: String) {
+        val request = HttpKit.getInstance().create(Api::class.java)
+        val observable: Observable<Any> = request.login(phone, password, 2, "")
+        observable.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : Observer<Any> {
+                override fun onComplete() {
+                }
+
+                override fun onSubscribe(d: Disposable) {
+                }
+
+                override fun onNext(t: Any) {
+                }
+
+                override fun onError(e: Throwable) {
+                }
+            })
     }
 }
